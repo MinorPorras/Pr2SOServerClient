52c637a99a00f64294ab68bab4a4d06b8cdb3969
Kevin <kvelezsalazar07@gmail.com>
2024-12-05 22:03:52 -0600

    Clone Fix

--- Cliente/obj/Release/net6.0-windows/Cliente.AssemblyInfo.cs
+++ Cliente/obj/Release/net6.0-windows/Cliente.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cliente")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79bc93c917f5a1f1c4e8363640167bf8c331c540")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cliente")]
